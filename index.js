b16ef2feb48c03f759d22c218ab85598186b1b40
ziniulian <ziniulian@163.com>
2019-10-20 22:10:41 +0800

    gx

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ LZR.load([
 // 服务的实例化
 var srv = new LZR.Node.Srv ({
 	ip: process.env.OPENSHIFT_NODEJS_IP || "0.0.0.0",
-	port: process.env.OPENSHIFT_NODEJS_PORT || 80
+	port: process.env.OPENSHIFT_NODEJS_PORT || 8080
 });
 
 // 需要用到的工具
